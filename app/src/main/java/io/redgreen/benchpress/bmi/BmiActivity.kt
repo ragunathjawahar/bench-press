7af97f12aa5b9c76e7af8bacc7b43084d5b31efe
Ragunath Jawahar <ragunathjawahar@gmail.com>
2019-01-13 10:30:12 +0530

    BMI, add XML layout and string resources.

--- app/src/main/java/io/redgreen/benchpress/bmi/BmiActivity.kt
+++ app/src/main/java/io/redgreen/benchpress/bmi/BmiActivity.kt
@@ -3,10 +3,19 @@ package io.redgreen.benchpress.bmi
 import android.os.Bundle
 import io.redgreen.benchpress.R
 import io.redgreen.benchpress.architecture.BaseActivity
+import kotlinx.android.synthetic.main.bmi_activity.*
 
 class BmiActivity : BaseActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.bmi_activity)
+    deleteMe()
+  }
+
+  private fun deleteMe() {
+    bmiTextView.text = 24.toString()
+    bmiCategoryTextView.text = getString(R.string.normal)
+    weightTextView.text = getString(R.string.template_weight_si, 0.0)
+    heightTextView.text = getString(R.string.template_height_si, 0.0)
   }
 }

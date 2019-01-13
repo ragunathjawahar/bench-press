def5332f4c67220d6623547adcbc0622b9ad3aa3
Ragunath Jawahar <ragunathjawahar@gmail.com>
2019-01-13 13:12:18 +0530

    Add UI for counter and hello, stranger example.

--- app/src/main/java/io/redgreen/benchpress/counter/CounterActivity.kt
+++ app/src/main/java/io/redgreen/benchpress/counter/CounterActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import io.redgreen.benchpress.R
 import io.redgreen.benchpress.architecture.BaseActivity
+import kotlinx.android.synthetic.main.counter_activity.*
 
 class CounterActivity : BaseActivity() {
   companion object {
@@ -16,5 +17,10 @@ class CounterActivity : BaseActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.counter_activity)
+    deleteMe()
+  }
+
+  private fun deleteMe() {
+    counterTextView.text = 0.toString()
   }
 }
